3e717394d9b9fe0e7adc58fbd693fee1c7fc17a8
kalamarfou <kalamarfou@ff33d22a-4d69-5570-0286-3a4e34c4edd2>
2010-05-26 14:04:48 +0000

    Copié collage massif de classes de décoration. Elles sont toutes faites et elles sont trop belles.

---
+++ DAL/Personalized/Role.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using DAL.Utils;
+
+namespace DAL.Personalized
+{
+    [MetadataType(typeof(RolePersonalization))]
+    public partial class Role
+    {
+
+    }
+
+    public class RolePersonalization
+    {
+        [LocalizedDisplayName("Name", NameResourceType = typeof(DAL.Resources.Role))]
+        [Required(ErrorMessageResourceName = "NameRequired", ErrorMessageResourceType = typeof(DAL.Resources.Role))]
+        public string Name { get; set; }
+
+        [LocalizedDisplayName("ShortName", NameResourceType = typeof(DAL.Resources.Role))]
+        [Required(ErrorMessageResourceName = "ShortNameRequired", ErrorMessageResourceType = typeof(DAL.Resources.Role))]
+        public string ShortName { get; set; }
+    }
+
+}
