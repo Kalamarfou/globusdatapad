8f320c1b25ef96fc749b1b56dd225573ea5b8c11
martin.filliau <martin.filliau@ff33d22a-4d69-5570-0286-3a4e34c4edd2>
2010-06-03 08:35:51 +0000

    Intégration base calendrier front office

---
+++ WebApp/Areas/FrontOffice/Controllers/CalendarApiController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Services.Event;
+using WebApp.Extensions;
+
+namespace WebApp.Areas.FrontOffice.Controllers
+{
+    public class CalendarApiController : Controller
+    {
+        //
+        // GET: /FrontOffice/CalendarApi/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public JsonResult GetWorldWideEvents(double start, double end)
+        {
+            IEventService service = new EventService();
+
+            int i;
+
+            List<DAL.WorldWideEvent> wwe = service.GetWorldWideEvents(GlobalUtils.ConvertFromUnixTimestamp(start), GlobalUtils.ConvertFromUnixTimestamp(end), 0, 100, out i);
+
+            var eventList = from e in wwe
+                            select new
+                            {
+                                id = e.Id,
+                                title = e.Name,
+                                start = e.StartDate.ToString("s"),
+                                end = e.EndDate.ToString("s"),
+                                allDay = false,
+                                className = "worldWideEvent",
+                                url = "/FrontOffice/Calendar/Details/" + e.Id.ToString()
+                            };
+
+            var rows = eventList.ToArray();
+
+            return Json(rows, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
--- WebApp/Extensions/GlobalUtils.cs
+++ WebApp/Extensions/GlobalUtils.cs
@@ -8,5 +8,17 @@ namespace WebApp.Extensions
     public class GlobalUtils
     {
         public const string applicationName = "GlobusDataPad";
+
+        /// <summary>
+        /// Convert from UNIX timestamp.
+        /// See http://codeclimber.net.nz/archive/2007/07/10/convert-a-unix-timestamp-to-a-.net-datetime.aspx
+        /// </summary>
+        /// <param name="timestamp">Timestamp</param>
+        /// <returns>DateTime</returns>
+        public static DateTime ConvertFromUnixTimestamp(double timestamp)
+        {
+            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return origin.AddSeconds(timestamp);
+        }
     }
 }
